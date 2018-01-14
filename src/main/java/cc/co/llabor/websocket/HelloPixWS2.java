31b2fdbc575e4f6aaaf3880c3e0055bcbb192523
Ge Sha! <gesha@uniposta.de>
2018-01-14 17:47:45 +0100

    POLO...ix ++

---
+++ src/main/java/cc/co/llabor/websocket/HelloPixWS2.java
@@ -0,0 +1,44 @@
+package cc.co.llabor.websocket;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+ 
+// https://stackoverflow.com/questions/26452903/javax-websocket-client-simple-example
+public class HelloPixWS2 {
+
+    public static void main(String[] args) {
+        try {
+            // open websocket
+            final WebsocketClientEndpoint_V2 clientEndPoint = new WebsocketClientEndpoint_V2(new URI("wss://api2.poloniex.com"));
+            
+
+ System.out.println(1);
+            
+            // add listener
+            clientEndPoint.addMessageHandler(new WebsocketClientEndpoint_V2.MessageHandler() {
+                public void handleMessage(String message) {
+                    System.out.println("<<<<<<<<"+message);
+                }
+            });
+System.out.println(2);            
+            
+
+            // send message to websocket
+//            clientEndPoint.sendMessage("{\"command\":\"subscribe\",\"channel\":1001}");
+//            clientEndPoint.sendMessage("{\"command\":\"subscribe\",\"channel\":1002}");
+//            clientEndPoint.sendMessage("{\"command\":\"subscribe\",\"channel\":1003}");
+//            clientEndPoint.sendMessage("{\"command\":\"subscribe\",\"channel\":\"USDT_BTC\"}");
+
+            // wait 5 seconds for messages from websocket
+            Thread.sleep(115000);
+            
+             
+            
+            Thread.sleep(50000);
+        } catch (InterruptedException ex) {
+            System.err.println("InterruptedException exception: " + ex.getMessage());
+        } catch (URISyntaxException ex) {
+            System.err.println("URISyntaxException exception: " + ex.getMessage());
+        }
+    }
+}
