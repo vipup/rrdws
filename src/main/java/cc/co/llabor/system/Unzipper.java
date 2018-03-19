fbf3d5cf7f8257a65300d84d5257866416dff35b
vipup <vasja0441@gMaIl.com>
2018-03-19 14:15:07 +0100

    BlockerErrors--

--- src/main/java/cc/co/llabor/system/Unzipper.java
+++ src/main/java/cc/co/llabor/system/Unzipper.java
@@ -3,13 +3,15 @@ package cc.co.llabor.system;
 import java.io.File;
 import java.io.FileInputStream; 
 import java.io.FileOutputStream;
-import java.io.IOException; 
+import java.io.IOException;
+import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream; 
 
 public class Unzipper { 
 	private String outForlderToUnzip;
-	private String inputZipArchiveFileName; 
+	private String inputZipArchiveFileName;
+	static final Logger log = Logger.getLogger(Unzipper.class.getName()); 
 	
 	    public Unzipper(String inName, String outName) {
 	    	this.inputZipArchiveFileName =inName;  
@@ -24,42 +26,55 @@ public class Unzipper {
 		public final void unzip() throws IOException{
 	    	byte[] buffer = new byte[1024];
 	    	//get the zip file content
-	    	ZipInputStream zis =
-	    		new ZipInputStream(new FileInputStream(inputZipArchiveFileName));
-	    	//get the zipped file list entry
-	    	ZipEntry ze = zis.getNextEntry();
-	    	while(ze!=null){
+	    	ZipInputStream zis = null;
+	    	try{
+	    		zis = new ZipInputStream(new FileInputStream(inputZipArchiveFileName));
+		    	//get the zipped file list entry
+		    	ZipEntry ze = zis.getNextEntry();
+		    	while(ze!=null){
+	
+		     	   String fileName = ze.getName();
+		            File newFile = new File(outForlderToUnzip + File.separator + fileName);
+	
+		            log.info( "file unzip : "+ newFile.getAbsoluteFile());
+	
+		             //create all non exists folders
+		             //else you will hit FileNotFoundException for compressed folder
+		             
+		             FileOutputStream fos = null ;
+		             try {
+		            	 new File(newFile.getParent()).mkdirs();
+		              
+			             fos= new FileOutputStream(newFile);
+		
+			             int len;
+			             while ((len = zis.read(buffer)) > 0) {
+			        		fos.write(buffer, 0, len);
+			             }
+		
+			             
+		             }finally {
+		            	 if(null!=fos)fos.close();
+		             }
+		             ze = zis.getNextEntry();
+		     	}
+	
+		        zis.closeEntry();
+	    	}finally { 
+	    		if(null!=zis) {
+	    			zis.close();
+	    		}
+         		
+	    	}
 
-	     	   String fileName = ze.getName();
-	            File newFile = new File(outForlderToUnzip + File.separator + fileName);
-
-	            System.out.println("file unzip : "+ newFile.getAbsoluteFile());
-
-	             //create all non exists folders
-	             //else you will hit FileNotFoundException for compressed folder
-	             new File(newFile.getParent()).mkdirs();
-
-	             FileOutputStream fos = new FileOutputStream(newFile);
-
-	             int len;
-	             while ((len = zis.read(buffer)) > 0) {
-	        		fos.write(buffer, 0, len);
-	             }
-
-	             fos.close();
-	             ze = zis.getNextEntry();
-	     	}
-
-	         zis.closeEntry();
-	     	zis.close();
-
-	     	System.out.println("Done"); 
+	     	
+	     	log.info( "Done"); 
 	    }
 	    
 	    public static void main(String[] args) throws IOException {
-		    String SRC = "/tmp/yyy/xxx.ZIP";
-		    String DST = "/tmp/zzz/"; // SourceFolder path	    	
-	    	Unzipper zTmp = new Unzipper(SRC, DST);
+		    String sSRC = "/tmp/yyy/xxx.ZIP";
+		    String sDST = "/tmp/zzz/"; // SourceFolder path	    	
+	    	Unzipper zTmp = new Unzipper(sSRC, sDST);
 	    	zTmp.unzip();
 	    }
 
