095c039e887bd6c95812ecbf0ab65d0decfd093c
i1 <i1@runlola.localdomain>
2019-09-16 01:46:37 -0700

    sleep for DDOS-Polo-server++

--- doforever.sh
+++ doforever.sh
@@ -4,6 +4,6 @@ do
 	export MAVEN_OPTS="-Xmx1531m -server -XX:+ExitOnOutOfMemoryError "
 	mv target/catalina.base/11/logs/rrdwsout.log   /var/log/rrd/$(date +%Y%m%d-%H%M%S)rrdwsout.log
 	mvn -o   exec:java -Dexec.mainClass=cc.co.llabor.websocket.WS2RRDPump  -Dcatalina.base=target/catalina.base/11 -Dnet.sf.jsr107cache.CacheFactory=cc.co.llabor.cache.BasicCacheFactory   -Dexec.classpathScope=test   -Dmaven.test.skip=true 
-
+	sleep 10
 done
 
