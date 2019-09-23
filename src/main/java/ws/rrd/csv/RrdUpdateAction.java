41d8e7a0c1679f58ed5220ca385c36b7f1e439a0
Bic Ed <devan.oppenheimer@uniposta.de>
2019-09-23 16:44:41 +0200

    FEATURE: x10 -> RRA:AVERAGE

--- src/main/java/ws/rrd/csv/RrdUpdateAction.java
+++ src/main/java/ws/rrd/csv/RrdUpdateAction.java
@@ -122,7 +122,7 @@ public class RrdUpdateAction implements Action {
 				"				RRA:AVERAGE:0.5:17:592 " +
 				"				RRA:AVERAGE:0.5:131:340 " +
 				"				RRA:AVERAGE:0.5:731:719 " +
-				"				RRA:AVERAGE:0.5:10000:273 " +
+				"				RRA:AVERAGE:0.5:10000:2730 " +
 				"				RRA:MAX:0.5:3:480 " +
 				"				RRA:MAX:0.5:17:592 " +
 				"				RRA:MAX:0.5:131:340 " +
